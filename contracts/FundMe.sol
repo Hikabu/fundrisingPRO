944d465851a2ba3c8b5ec63c3dfdd115a1461c0d
Hikabu <eatingvulfrik@gmail.com>
2025-08-17 15:50:04 +0700

    deployed

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,13 +4,19 @@ pragma solidity ^0.8.24;
 import {converter} from "./converter.sol";
 
 contract FundMe{
+
   //if in library fucntions first parametnr is uint256 allow me to call them as they are methods of uint256
   //solidity  just rewriting msg.value.getConverter() to converter.getConversion(msg.value)
   using converter for uint256;
 
-    uint256 public minUsd = 4e18;
+    uint256 public minUsd = 5e18;
     address[] public funders;
     mapping(address => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+  constructor() {
+      owner = msg.sender;
+  }
     function fund() public payable { 
         require(msg.value.getConversion() >= minUsd,  "didn't send enough ETH");
         funders.push(msg.sender);
@@ -18,7 +24,7 @@ contract FundMe{
         //but can undo
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner{
       //for loop
       for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
         address funder = funders[funderIndex];
@@ -34,8 +40,14 @@ contract FundMe{
       // //send only if there is require
       // bool sendSuccess = payable (msg.sender).send(address(this).balance);
       // require(sendSuccess, "send failed");
+      
       //call( calling virtually wothout  abi)
       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
       require(callSuccess, "Call failed");
+    }
+    modifier onlyOwner() {
+      require(msg.sender == owner, "Must be the owner");
+      //whatever in the function
+      _;
     }
 }
